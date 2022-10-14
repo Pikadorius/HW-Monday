b7ed10eecbc1b2446bdd52a1f1cce9443c01397b
Pikadorius <karpukea@yandex.by>
2022-10-14 14:22:52 +0300

    experiments for deploy

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -34,7 +34,7 @@ export const message0: MessageType = {
         name: 'Some Name',  // можно менять
     },
     message: {
-        text: 'some tee textsome textsome textsome textsome textsome text', // можно менять
+        text: 'Я просто потанцевать хотел...', // можно менять
         time: '22:00', // можно менять
     },
 }
@@ -45,7 +45,7 @@ export const friendMessage0: MessageType = {
         name: 'Friend Name', // можно менять
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
+        text: 'ТЫ ДИБИЛ? ', // можно менять
         time: '22:00', // можно менять
     },
 }
